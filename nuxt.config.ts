3328c1292562e3d59dfbbb2d9e6565b01dbe2965
joseplin <joseplin@foxmail.com>
2026-03-18 16:01:23 +0800

    feat: 添加本地字体提供者配置到 Nuxt 配置文件

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,7 +1,12 @@
+import type { provide } from "vue";
+
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   css: ["~/assets/css/main.css"],
   modules: ["@nuxt/ui"],
   compatibilityDate: "2025-07-15",
   devtools: { enabled: true },
+  fonts: {
+    provider: "local",
+  },
 });
